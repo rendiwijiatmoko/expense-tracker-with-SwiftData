4b5e7b566597f570021a86add68dd1a3d7fbdb74
Rendi Wijiatmoko <rendiwijiatmoko97@gmail.com>
2023-10-17 16:34:24 +0700

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Rendi Wijiatmoko on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
